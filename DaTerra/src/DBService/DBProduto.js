655414244fa3873177f0ef78aeb864e1dd2dcb1b
CarlosCamuzzi <carloscamuzzi.hs@gmail.com>
2022-10-11 21:38:44 -0300

    layout produto

--- DaTerra/src/DBService/DBProduto.js
+++ DaTerra/src/DBService/DBProduto.js
@@ -2,21 +2,16 @@ import Database from './DBServices';
 
 const DB_EXEC = Database.getConnection();
 
-export const cadastrarProduto=async(produto)=>{
-let results = await DB_EXEC('INSERT INTO Produtos'+
-'(nomeProduto,preco,tipoEmbalagem,quantidadeEstoque,categoriaProduto,descricao)VALUES(?,?,?,?,?)',[produto.nomeProduto,produto.preco,produto,tipoEmbalagem,produto.quantidadeEstoque,produto.categoriaProduto,produto.descricao]);
- return rowsAffected;
-
-
-
+export const cadastrarProduto = async (produto) => {
+  let results = await DB_EXEC('INSERT INTO Produtos' +
+    '(nomeProduto,preco,tipoEmbalagem,quantidadeEstoque,categoriaProduto,descricao)VALUES(?,?,?,?,?)', [produto.nomeProduto, produto.preco, produto, tipoEmbalagem, produto.quantidadeEstoque, produto.categoriaProduto, produto.descricao]);
+  return rowsAffected;
 }
 
 
-
-
 //Será um metodo para  chamar quando renderizar a tela de loja do produtor,e mostrar todos os produtos daquele usuário de acordo com a sua ID.
-export const getProdutosUsuario= async (idUsuario)=>{
-let result = await DB_EXEC("SELECT *FROM Produtos WHERE id=?;",[idUsuario])
+export const getProdutosUsuario = async (idUsuario) => {
+  let result = await DB_EXEC("SELECT *FROM Produtos WHERE id=?;", [idUsuario])
 
 
 
