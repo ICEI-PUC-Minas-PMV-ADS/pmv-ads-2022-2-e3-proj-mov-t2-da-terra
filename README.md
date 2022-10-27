8c406f364855887c150dc4279378f7fd1140eaa8
CarlosCamuzzi <carloscamuzzi.hs@gmail.com>
2022-10-27 10:35:18 -0300

    nova versao

---
+++ README.md
@@ -0,0 +1,55 @@
+<h1 align="center"># DaTerra</h1>
+
+ ![logo300x300](https://github.com/ICEI-PUC-Minas-PMV-ADS/pmv-ads-2022-2-e3-proj-mov-t2-da-terra/blob/main/entregas/images/logo/logo300x300.png)
+
+`Análise e Desenvolvimento de Sistemas`
+
+`Desenvolvimento de Aplicação Mobile`
+
+`3° Eixo`
+
+DaTerra tem como objetivo conectar pequenos produtores de orgânicos com pessoas que buscam uma alimentação saudável.
+
+## Integrantes
+
+* Ana Paula Buchholz
+* Carlos Hilario Siqueira Camuzzi
+* Ezequiel Silva de Souza Almeida
+* Gabriel Antônio Lopes Costa 
+* Ila Feitosa da Nóbrega 
+* Júlia de Oliveira Sartori 
+
+
+## Orientador
+
+* Mateus Curcino de Lima
+
+## Instruções de utilização
+
+Assim que a primeira versão do sistema estiver disponível, deverá complementar com as instruções de utilização. Descreva como instalar eventuais dependências e como executar a aplicação.
+
+# Documentação
+
+<ol>
+<li><a href="docs/01-Documentação de Contexto.md"> Documentação de Contexto</a></li>
+<li><a href="docs/02-Especificação do Projeto.md"> Especificação do Projeto</a></li>
+<li><a href="docs/03-Metodologia.md"> Metodologia</a></li>
+<li><a href="docs/04-Projeto de Interface.md"> Projeto de Interface</a></li>
+<li><a href="docs/05-Arquitetura da Solução.md"> Arquitetura da Solução</a></li>
+<li><a href="docs/06-Template Padrão da Aplicação.md"> Template Padrão da Aplicação</a></li>
+<li><a href="docs/07-Programação de Funcionalidades.md"> Programação de Funcionalidades</a></li>
+<li><a href="docs/08-Plano de Testes de Software.md"> Plano de Testes de Software</a></li>
+<li><a href="docs/09-Registro de Testes de Software.md"> Registro de Testes de Software</a></li>
+<li><a href="docs/10-Plano de Testes de Usabilidade.md"> Plano de Testes de Usabilidade</a></li>
+<li><a href="docs/11-Registro de Testes de Usabilidade.md"> Registro de Testes de Usabilidade</a></li>
+<li><a href="docs/12-Apresentação do Projeto.md"> Apresentação do Projeto</a></li>
+<li><a href="docs/13-Referências.md"> Referências</a></li>
+</ol>
+
+# Código
+
+<li><a href="src/README.md"> Código Fonte</a></li>
+
+# Apresentação
+
+<li><a href="presentation/README.md"> Apresentação da solução</a></li>
---
+++ WebApi/Controllers/ProdutosController.cs
@@ -0,0 +1,128 @@
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApi.Data;
+using WebApi.Models;
+using WebApi.ViewModel;
+
+namespace WebApi.Controllers
+{
+  [ApiController]
+  [Route(template: "v1")]
+  public class ProdutosController : ControllerBase
+  {
+
+    // GET 
+    [HttpGet(template: "produtos/{id}")]
+    public async Task<IActionResult> GetProdutoAsync(
+        [FromServices] AppDbContext context,
+        [FromRoute] int id)
+    {
+      var produto = await context.Produtos
+      .AsNoTracking()
+      .FirstOrDefaultAsync(x => x.Id == id);
+
+      string produtoJson = JsonSerializer.Serialize(produto);
+
+      return produto == null ? NotFound() : Ok(produtoJson);
+    }
+
+    // POST
+    [HttpPost(template: "produtos")]
+    public async Task<IActionResult> PostProdutoAsync(
+      [FromServices] AppDbContext context,
+      [FromBody] CreateProdutoViewModel model)
+    {
+      if (!ModelState.IsValid)
+        return BadRequest();
+      
+      var produto = new Produto()
+      {
+        Nome = model.Nome,  
+        Preco = model.Preco,
+        Embalagem = model.Embalagem,
+        Estoque = model.Estoque,
+        Categoria = model.Categoria,
+        Descricao = model.Descricao,
+        DataCadastro = model.DataCadastro
+      };
+     
+      try
+      {
+        // aqui context.AddAsync(produto);
+        await context.AddAsync(produto);
+        await context.SaveChangesAsync();
+
+        return Created($"v1/produtos/{produto.Id}", produto);
+      }
+      catch (System.Exception)
+      {
+        return BadRequest();
+      }
+    }
+
+
+    // PUT
+    [HttpPut(template: "produtos/{id}")]
+    public async Task<IActionResult> PutPessoaAsync(
+            [FromServices] AppDbContext context,
+            [FromBody] CreateProdutoViewModel model,
+            [FromRoute] int id)
+    {
+      if (!ModelState.IsValid) return BadRequest();
+
+      var produto = await context.Produtos
+      .FirstOrDefaultAsync(x => x.Id == id);
+
+      if (produto == null)
+        return NotFound();
+
+      try
+      {
+        produto.Nome = model.Nome;
+        produto.Preco = model.Preco;
+        produto.Embalagem = model.Embalagem;
+        produto.Estoque = model.Estoque;
+        produto.Categoria = model.Categoria;
+        produto.Descricao = model.Descricao;
+        // produto.DataCadastro = model.DataCadastro;
+
+        context.Produtos.Update(produto);
+        await context.SaveChangesAsync();
+
+        return Ok(produto);
+      }
+      catch (System.Exception)
+      {
+        return BadRequest();
+      }
+    }
+
+    // DELETE
+    [HttpDelete(template: "produtos/{id}")]
+    public async Task<IActionResult> DeleteProdutoAsync(
+        [FromServices] AppDbContext context,
+        [FromRoute] int id)
+    {
+      var produto = await context.Produtos
+      .FirstOrDefaultAsync(x => x.Id == id);
+
+      try
+      {
+        context.Produtos.Remove(produto);
+        await context.SaveChangesAsync();
+
+        return Ok();
+      }
+      catch (System.Exception)
+      {
+        return BadRequest();
+      }
+    }
+  }
+
+
+
+
+
+}
