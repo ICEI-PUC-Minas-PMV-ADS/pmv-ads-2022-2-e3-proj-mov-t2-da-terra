26c3b292e28a8cb9643e16f74e223d86560d6b45
Gabriel <gabrielpuneco@gmail.com>
2022-11-08 20:52:06 -0300

    Implementando o método GetAllProdutos para buscar todos os produtos do produtor que está logado

--- WebApi/Controllers/ProdutosController.cs
+++ WebApi/Controllers/ProdutosController.cs
@@ -17,7 +17,10 @@ namespace WebApi.Controllers
     {
       _context = context;
     }
-    
+    public ProdutosController()
+    {
+      
+    }
 
     // GET 
     /*
@@ -114,8 +117,6 @@ namespace WebApi.Controllers
       {
         return BadRequest();
       }
-
-      
       
       try
       {
@@ -161,11 +162,14 @@ namespace WebApi.Controllers
     public async Task<IActionResult> GetAllProdutoAsync(
         [FromServices] AppDbContext context)
     {
-      var produto = await context.Produtos.ToListAsync();
-
-      // string produtoJson = JsonSerializer.Serialize(produto);
-
-      return produto == null ? NotFound() : Ok(produto);
+      var produtor =  await _context.Produtores.FirstOrDefaultAsync(s => s.Nome == User.Identity.Name);
+      //Acha os produtos de acordo com a Id do usuário logado,aonde pega a a chave FK na tabela Produtor
+      var produtosFind = context.Produtos.Where(a=>a.ProdutorId==produtor.Id);
+      /*"Tranforma" o tipo IQueryaable acima, que é o retorno dos produtos achados de tal usuário logado,para uma lista
+      De produtos a serem retornados*/
+      var produtos =  await produtosFind.ToListAsync();
+      //Retorna os produtos achados para a API,para ser consumida na aplicação REACT
+      return produtos == null ? NotFound() : Ok(produtos);
     }
 
     // POST
