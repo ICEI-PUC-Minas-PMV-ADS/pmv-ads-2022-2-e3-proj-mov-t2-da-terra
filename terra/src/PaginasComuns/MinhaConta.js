4670beb36928a2205bca8a507ada55b430d78046
Ana Paula Buchholz <anapbuchholz@gmail.com>
2022-11-09 20:44:53 -0300

    inserindo alterações na tela MinhaConta

--- terra/src/PaginasComuns/MinhaConta.js
+++ terra/src/PaginasComuns/MinhaConta.js
@@ -1,5 +1,5 @@
 import React,{useContext} from 'react';
-import { Text } from "react-native";
+import { Text, StyleSheet } from "react-native";
 
 import Body from "../Componentes/Body";
 import Container from '../Componentes/Container';
@@ -11,14 +11,74 @@ const MinhaConta = () => {
     const {user,setUser} = useContext(AuthContext)//Provider com as informações do usuário logado
     return (
         <Body>
-            <Text>Nome: {user.nome}</Text>
+            {/* FotoDoUsuário */}
+        <Image
+          style={styles.foto}
+          source={require("../assets/DATERRA-COMPLETO-800X1050.png")} //inserção da foto do usuário
+        />
+            <Text style={styles.viewTexto}>Nome: {user.nome}</Text>
 
-            <Text>Email: {user.email}</Text>
+            <Text style={styles.viewTexto}>Email: {user.email}</Text>
             
 
   
         </Body>
     );
 }
+
+const styles = StyleSheet.create({
+    containerProdutos: {
+      borderRadius: 10,
+      padding: 10,
+      margin: 5,
+      backgroundColor: "#fff",
+      elevation: 5,
+    },
+    foto: {
+        height: 190,
+        width: 145,
+        padding: 10,
+        marginBottom: 30,
+        alignSelf: "center",
+      },
+    viewTexto: {
+        width: 350,
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: -11,
+      },
+    // Imagem
+    viewImg: {
+      flexDirection: "row",
+      justifyContent: "center",
+    },
+    textoCard: {
+      marginVertical: 3,
+      fontSize: 13,
+      fontWeight: "bold",
+      letterSpacing: 0.75,
+    },
+    viewVerMaisProdutos: {
+      marginVertical: 15,
+      flexDirection: "row",
+      justifyContent: "space-evenly",
+    },
+    imgPlus: {
+      width: 101,
+      height: 83,
+      borderTopLeftRadius: 7,
+      borderTopRightRadius: 7,
+    },
+    img: {
+      maxWidth: 228,
+      maxHeight: 175,
+      flexGrow: 1,
+      flexShrink: 1,
+      borderTopLeftRadius: 12,
+      borderTopRightRadius: 12,
+      marginRight: 10,
+      alignSelf: "center",
+    }
+});
 
 export default MinhaConta;
