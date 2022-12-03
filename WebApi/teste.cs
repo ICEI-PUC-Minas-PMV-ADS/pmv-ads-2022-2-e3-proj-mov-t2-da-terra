5a2333eb037b53fc1db4ff36de5841176a20ce68
CarlosCamuzzi <carloscamuzzi.hs@gmail.com>
2022-12-03 14:58:04 -0300

    img testes

--- WebApi/teste.cs
+++ WebApi/teste.cs
@@ -1,17 +1,17 @@
-﻿using WebApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.IO;
-using Newtonsoft.Json;
-namespace WebApi;
+﻿// using WebApi.Models;
+// using System;
+// using System.Collections.Generic;
+// using System.Globalization;
+// using System.IO;
+// using Newtonsoft.Json;
+// namespace WebApi;
 
 
 
-    Venda venda1 = new Venda(1,"dadsas","afsdfas");
-    Venda venda2 = new Venda(2, "Mterial sala","20:21");
-    public List<Venda> vendas = new List<Venda>();
-    vendas.Add(venda1);
+//     Venda venda1 = new Venda(1,"dadsas","afsdfas");
+//     Venda venda2 = new Venda(2, "Mterial sala","20:21");
+//     public List<Venda> vendas = new List<Venda>();
+//     vendas.Add(venda1);
     
-    string serializado = JsonConvert.SerializeObject(vendas,Formatting.Indented);
-    Console.WriteLine(serializado);
+//     string serializado = JsonConvert.SerializeObject(vendas,Formatting.Indented);
+//     Console.WriteLine(serializado);
